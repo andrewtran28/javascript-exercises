da09e01f0b7b34a69469d91045f13a04868b43b8
andrewtran28 <tran.andrew@outlook.com>
2024-07-22 09:39:07 -0400

    Complete exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+
+    let array = args[0];
+    let filteredArray = array;
+    let removeValue;
+
+    for (let i = 1; i < args.length; i++) {
+        removeValue = args[i];
+        filteredArray = filteredArray.filter((filteredArray) => filteredArray !== removeValue);
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
