39e3813037b6ae6c8a12d7b018c6652260a115c7
andrewtran28 <tran.andrew@outlook.com>
2024-07-22 10:26:35 -0400

    Complete exercise five.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+
+    let sum = 0;
+    let tempInt = 0;
+    
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+
+    if (Number.isInteger(int1) == 0 || Number.isInteger(int2) == 0) {
+        return "ERROR";
+    }
+
+    //Check to see if second argument is smaller than first argument, then swap them if so.
+    if (int2 < int1) {
+        tempInt = int2;
+        int2 = int1;
+        int1 = tempInt;
+    }
+
+    for (let i = int1; i <= int2; i++) {
+        sum += i
+    }
+
+    return sum;
 
 };
 
