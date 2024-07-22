354556e307a29af66cdfc3aaf11f2056ee6877f6
andrewtran28 <tran.andrew@outlook.com>
2024-07-22 08:37:24 -0400

    Complete exercise three.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let strLength = string.length;
+let reverseStr = "";
+
+for (let i = strLength-1; i >= 0; i--) {
+    reverseStr += string.charAt(i);
+}
+
+return reverseStr;
 };
 
 // Do not edit below this line
