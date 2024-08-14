e15bda2d9763e5814cbacdaeece487da31fd60f7
andrewtran28 <tran.andrew@outlook.com>
2024-08-14 18:40:56 -0400

    Completed exercise eight.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accum, current) => accum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let total = 0;
+  for (i = 0; i < exp; i++) {
+    total *= base;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n = 0) return 1;
+
+  let product = 1;
+  for (i = 1; i <= n; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
