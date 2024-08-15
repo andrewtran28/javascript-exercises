f4925fb28e0bc7ac75deb76f1daf3694aa70d44b
andrewtran28 <tran.andrew@outlook.com>
2024-08-15 08:14:43 -0400

    Completed exercise nine.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const rawString = string
+        .toUpperCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
+
+    const reverseString = rawString
+        .split('')
+        .reverse()
+        .join('');
+
+    return rawString === reverseString;
 };
 
 // Do not edit below this line
